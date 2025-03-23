3a6f2785ecf0588bc35f51f5b34355114d4324b2
Robeel Bhatti <robeelbhatti19@gmail.com>
2025-03-23 13:46:14 -0400

    chore: remove unused import.
    .

--- src/main/java/io/robeel/bhatti/taskFlow/TaskFlowApplication.java
+++ src/main/java/io/robeel/bhatti/taskFlow/TaskFlowApplication.java
@@ -1,6 +1,5 @@
 package io.robeel.bhatti.taskFlow;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
