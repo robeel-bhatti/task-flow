3bfdefc0532796371d21901ab49ba294d1c275eb
Robeel Bhatti <robeelbhatti19@gmail.com>
2025-04-05 20:25:15 -0400

    build: add husky and commitlint configuration.
    This is to ensure all commits to this repository conform to the conventional commit standard.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,8 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+const Configuration = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "subject-full-stop": [0, "always"],
+  }
+};
+
+export default Configuration;
