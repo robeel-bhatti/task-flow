21e5291596ac921745b16602ed6d0818877c2a8f
Robeel Bhatti <robeelbhatti19@gmail.com>
2025-04-30 23:47:37 -0400

    feat: add angular/material library to have dialog functionality.

--- task-flow/src/app/home/home.component.ts
+++ task-flow/src/app/home/home.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  readonly dialog = inject(MatDialog);
 
+  ngOnInit(): void {
+    this.dialog.open(LoginPageComponent);
+  }
 }
